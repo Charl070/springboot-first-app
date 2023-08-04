5e592ed270c2c32babfcb72dcd86ed628340ac3e
Charles Anesu Kombo <62628677+Charl070@users.noreply.github.com>
2023-08-04 13:53:19 +0200

    feat: now getting a product by the id

--- src/main/java/com/springboot/springboot/controllers/ProductsController.java
+++ src/main/java/com/springboot/springboot/controllers/ProductsController.java
@@ -1,7 +1,10 @@
 package com.springboot.springboot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -18,5 +21,16 @@ public class ProductsController {
     public List<Product> getAllProducts() {
 
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") String productId) {
+
+        Product product =productService.getProductById(productId);
+        if(product == null) {
+            return new ResponseEntity<> (product, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<> (product, HttpStatus.OK);
     }
 }
